4ad81178c6bbca662c0af65ba45955d69ad0e92a
Vitalii Trachenko <vitaltrachenko@gmail.com>
2019-05-23 12:36:50 +0300

    chore (pg-restore-script)
    update pg-restore-script to use specific or latest pg_dump file

--- scripts/pg-restore-from-s3.sh
+++ scripts/pg-restore-from-s3.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# S3_PATH - S3 bucket name including prefix (folder) without actual file name (example: bucket_name/some/path/pg_dump_XXXX-XX-XX_XX-XX-XX.gz)
+# S3_PATH - S3 bucket name including prefix (folder) without actual file name (example: bucket_name/some/path)
+# BACKUP_FILE_NAME - S3 bucket file name (example: pg_dump_XXXX-XX-XX_XX-XX-XX.gz)
 
 # PG settings
-# PG_HOST
-# PG_PORT
-# PG_USERNAME
-# PG_PASSWORD
-# PG_DATABASE
+# POSTGRES_URI - postgres://username:password@host:5432/databasename
 
-aws s3 cp --quiet s3://${S3_PATH} - | \
- pg_restore --verbose --clean --if-exists --exit-on-error -d postgres://${PG_USERNAME}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}
+if [ "${BACKUP_FILE_NAME}x" == "x" ]; then
+    LATEST_BACKUP_FILE_NAME=$(aws s3 cp s3://${S3_PATH}/latest-postgresql-dump-version.txt - )
+    echo "Restoring from -> l${LATEST_BACKUP_FILE_NAME}"
+    aws s3 cp --quiet s3://${S3_PATH}/${LATEST_BACKUP_FILE_NAME} - | \
+        pg_restore --verbose --clean --if-exists --exit-on-error -d ${POSTGRES_URI}
+
+else
+    echo "Restoring from -> l${BACKUP_FILE_NAME}"
+    aws s3 cp --quiet s3://${S3_PATH}/${BACKUP_FILE_NAME} - | \
+        pg_restore --verbose --clean --if-exists --exit-on-error -d ${POSTGRES_URI}
+fi
