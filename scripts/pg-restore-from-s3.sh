6bf675db2a3a376b0e7f9438044edc9e454712b2
Vitalii Trachenko <vitaltrachenko@gmail.com>
2019-05-23 14:23:34 +0300

    refactor
    improve pg restore script

--- scripts/pg-restore-from-s3.sh
+++ scripts/pg-restore-from-s3.sh
@@ -7,13 +7,9 @@
 # POSTGRES_URI - postgres://username:password@host:5432/databasename
 
 if [ "${BACKUP_FILE_NAME}x" == "x" ]; then
-    LATEST_BACKUP_FILE_NAME=$(aws s3 cp s3://${S3_PATH}/latest-postgresql-dump-version.txt - )
-    echo "Restoring from -> l${LATEST_BACKUP_FILE_NAME}"
-    aws s3 cp --quiet s3://${S3_PATH}/${LATEST_BACKUP_FILE_NAME} - | \
-        pg_restore --verbose --clean --if-exists --exit-on-error -d ${POSTGRES_URI}
-
-else
-    echo "Restoring from -> l${BACKUP_FILE_NAME}"
-    aws s3 cp --quiet s3://${S3_PATH}/${BACKUP_FILE_NAME} - | \
-        pg_restore --verbose --clean --if-exists --exit-on-error -d ${POSTGRES_URI}
+    BACKUP_FILE_NAME=$(aws s3 cp s3://${S3_PATH}/latest-postgresql-dump-version.txt - )
 fi
+
+echo "Restoring from -> l${BACKUP_FILE_NAME}"
+aws s3 cp --quiet s3://${S3_PATH}/${BACKUP_FILE_NAME} - | \
+    pg_restore --verbose --clean --if-exists --exit-on-error -d ${POSTGRES_URI}
