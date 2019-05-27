5e4596adcd3f5cab3e0513556e79a825ec6180b4
Vitalii Trachenko <vitaltrachenko@gmail.com>
2019-05-27 09:47:32 +0300

    chore (restore)
    update mysql restore script to maintain latest backups as default

--- scripts/mysql-restore-from-s3.sh
+++ scripts/mysql-restore-from-s3.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# S3_PATH - should contain S3 bucket name and path to backup file (example: bucket_name/some/path/mysqldump_XXXX-XX-XX_XX-XX-XX.sql.gz)
+# S3_PATH - S3 bucket name including prefix (folder) without actual file name (example: bucket_name/some/path)
+# BACKUP_FILE_NAME - S3 bucket file name (example: pg_dump_XXXX-XX-XX_XX-XX-XX.gz)
 
+# MYSQL settings
 #MYSQL_USER
 #MYSQL_HOST
 #MYSQL_PASSWORD
 #MYSQL_DATABASE
 
-echo "Restore dump for database '${MYSQL_DATABASE}'..."
-aws s3 cp s3://${S3_PATH} - | zcat | mysql -u"${MYSQL_USER}" -h"${MYSQL_HOST}" -p"${MYSQL_PASSWORD}" ${MYSQL_DATABASE}
+if [ "${BACKUP_FILE_NAME}x" == "x" ]; then
+    BACKUP_FILE_NAME=$(aws s3 cp s3://${S3_PATH}/latest-mysql-dump-version.txt - )
+fi
+echo "Restoring from -> ${BACKUP_FILE_NAME}"
+aws s3 cp s3://${S3_PATH}/${BACKUP_FILE_NAME} - | zcat | mysql -u"${MYSQL_USER}" -h"${MYSQL_HOST}" -p"${MYSQL_PASSWORD}" ${MYSQL_DATABASE}
+
